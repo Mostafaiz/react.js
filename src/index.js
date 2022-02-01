6c16f812bff57849361e17ac63e965e5f220f222
mostafaiz <mostafaiz@live.com>
2022-02-01 10:21:07 +0330

    ok

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const App = () =>{
-    return(
-        <div>
+// const App = () =>{
+//     return React.createElement('h1',{className:'title'},'Hello World');
+// }
+
+const App = () =>(
+    <div className='one'>
             <h1>Hello mostafa</h1>
-        </div>
-    )
-}
+            <h2>Hello mostafa</h2>
+    </div>
+ )
+
 
 ReactDom.render(<App/>,document.getElementById('root'))
