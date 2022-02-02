89e15b85fa9e9072b54a4fa99427b52bde7e4dc6
mehdi <mehdi0437@gmail.com>
2022-02-02 19:07:13 +0330

    ide jadidam

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import ReactDom from 'react-dom';
 const App = () =>{
     return(
         <div>
-            <h1>Hello mostafa</h1>
+            <h1>Hello mehdi 200</h1> 
+            {/* من مصطفی رو تغییر دادم */}
+
+            <h1>Hello mostafa 200</h1> 
+            {/* من مصطفی رو تغییر دادم */}
+
         </div>
     )
 }
