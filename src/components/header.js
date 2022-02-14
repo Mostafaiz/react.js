46c5024d566645879bb7a7e901a1e112e4dfba37
mostafaiz <mostafaiz@live.com>
2022-02-14 15:48:27 +0330

    lec12

--- src/components/header.js
+++ src/components/header.js
@@ -2,32 +2,36 @@ import React,{Component} from 'react';
 
 class Header extends Component {
 
-    // inputChangeHandler(){
-    //     console.log('input changed')
-    // }
-
-    inputChangeHandler(event){
-        console.log(event.target.value);
-        //meghdar inputi ke darim
-
-
-        console.log('hey');
-        // be sorat sabet mikonim
+    state ={
+        name:'francis',
+        title:'the keyword are:',
+        keywords:'',
+        count:0,
     }
 
+    inputChangeHandler=(event)=>{
+        this.setState({
+        keywords: event.target.value
+        })
+    }
+
+    addOne(){
+        this.setState({ count: this.state.count + 1 })
+    }
 
     render(){
         return (
 
-            <header
-            // onClick={() => console.log('iwas clicked')}
-            >
+            <header>        
             <div className='logo'>Logo</div>
             <input
             onChange={this.inputChangeHandler}
-            // onChange={this.inputChangeHandler}
-
-            ></input>
+            />
+            <div>{this.state.title}</div>
+            <div>{this.state.keywords}</div>
+            <br/>
+            <div>{this.state.count}</div>
+            <button onClick={()=> this.addOne()}>Add One</button>  
            </header>
         )
     } 
